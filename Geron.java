62175078da2a365ca35d8ac71b3ca3a8f7da3b33
Svartov <100573588+Svartov@users.noreply.github.com>
2022-08-30 20:13:52 +0200

    Update Geron.java

--- Geron.java
+++ Geron.java
@@ -1,12 +1,12 @@
 public class Geron {
     public static void main(String[] args) {
-        float a = 0.3F;
-        float b = 0.4F;
-        float c = 0.5F;
-        float p;
-        float s;
+        double a = 0.3;
+        double b = 0.4;
+        double c = 0.5;
+        double p;
+        double s;
         p = (a + b + c) / 2;
-        s = (float)Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
         System.out.println(s);
     }
